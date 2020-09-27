9fbd8dbe5961a7c55f8c74729b5ef351f1bcde07
h595488 <69847918+h595488@users.noreply.github.com>
2020-09-27 19:19:46 +0200

    Add files via upload

---
+++ Innlevering 2/dat100-javainnlevering2/src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -0,0 +1,70 @@
+package no.hvl.dat100.lab6.matriser;
+
+public class Matriser {
+
+	// a)
+	public static void skrivUt(int[][] matrise) {
+
+		for (int[] rad : matrise) {
+			for (int x : rad) {
+				System.out.println();
+				System.out.print(x + " ");
+			}
+		}
+	}
+
+	// b)
+	public static String tilStreng(int[][] matrise) {
+
+		String str = "";
+		for (int[] rad : matrise) {
+			for (int x : rad) {
+				str += x + " ";
+				if (x % 3 == 0) {
+					str += "\n";
+				}
+
+			}
+		}
+		return str;
+
+	}
+
+	// c)
+	public static int[][] skaler(int tall, int[][] matrise) {
+
+		int[][] nyMatrise = new int[matrise.length][matrise[0].length];
+		for (int y = 0; y < matrise.length; y++)
+			for (int x = 0; x < matrise[y].length; x++)
+				nyMatrise[y][x] = matrise[y][x] * tall;
+		return nyMatrise;
+	}
+
+	// d)
+	public static boolean erLik(int[][] a, int[][] b) {
+
+		for (int i = 0; i < a.length; i++)
+			for (int j = 0; j < a[i].length; j++) {
+				if (a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		return true;
+	}
+
+	// e)
+	public static int[][] speile(int[][] matrise) {
+
+		// TODO
+		throw new UnsupportedOperationException("speile ikke implementert");
+
+	}
+
+	// f)
+	public static int[][] multipliser(int[][] a, int[][] b) {
+
+		// TODO
+		throw new UnsupportedOperationException("multipliser ikke implementert");
+
+	}
+}
